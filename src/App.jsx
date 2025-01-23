44b62359099c2017e62d80a21a7347a96d350e43
saqibhussain789 <shkb2003@gmail.com>
2025-01-23 17:30:16 +0500

    Added new files and updated existing ones

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Hero from "./components/Hero/Hero";
 import "./App.css";
 import Companies from "./components/Companies/Companies";
 import Residencies from "./components/Residencies/Residencies";
+import Value from "./components/Value/Value";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,6 +22,8 @@ function App() {
       </div>
       <Companies />
       <Residencies/>
+      <Value/>
+      <Contact/>
     </>
   );
 }
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <section className='c-wrapper'>
+      <div className="paddings innerWidth flexCenter c-container">
+        {/* left side */}
+        <div className="c-left">
+         
+        </div>
+
+        {/* right side */}
+        <div className="c-right">
+          <div className="image-container">
+            <img src="./contact.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
